6b58a215b51391930ef3ae5ec35d86241ba26af1
TheLeaderAhn <88701220+TheLeaderAhn@users.noreply.github.com>
2022-08-07 15:01:15 +0900

    chapter1 init
    프로젝트를 위한 준비

---
+++ ex1/src/main/java/org/zerock/ex1/ServletInitializer.java
@@ -0,0 +1,16 @@
+package org.zerock.ex1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+        System.out.println("Hello AJY");
+
+        return application.sources(Ex1Application.class);
+    }
+
+}
