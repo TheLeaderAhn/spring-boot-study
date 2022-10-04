6a4e04ff05990b8b8241eecd3fb55467360ab89a
TheLeaderAhn <lux-veritas@naver.com>
2022-10-04 23:05:46 +0900

    chapter5 project init

---
+++ no.02/ch5board/src/main/java/io/ahnjy/ch5board/Ch5boardApplication.java
@@ -0,0 +1,13 @@
+package io.ahnjy.ch5board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch5boardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch5boardApplication.class, args);
+    }
+
+}
---
+++ no.02/ch5board/src/test/java/io/ahnjy/ch5board/Ch5boardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ahnjy.ch5board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch5boardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
